a7488cf77870fbfb34028c8b9d88aa085659ff9e
dhk767 <dhk767@gmail.com>
2022-10-03 22:41:53 +0900

    Initial Commit

---
+++ war-challenge/war_challengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  war_challengeApp.swift
+//  war-challenge
+//
+//  Created by 김동현 on 2022/10/03.
+//
+
+import SwiftUI
+
+@main
+struct war_challengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
